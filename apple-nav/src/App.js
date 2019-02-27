bdc278f557eeee4fedcedb132a5881767a77acf3
Francis Chen <fncis.chen@gmail.com>
2019-02-27 08:58:33 -0800

    added react router packages

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import NavWrapper from './Components/NavWrapper';
+import Body from "./Components/Body";
+
+import { Route } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <NavWrapper />
+
+          <Route path="/devices/:mac" component={Body}/>
+          <Route path="/devices/:iPad" component={Body}/>
+          <Route path="/devices/:iPhone" component={Body}/>
+          <Route path="/devices/:Watch" component={Body}/>
+          <Route path="/devices/:TV" component={Body}/>
+
       </div>
     );
   }
